9c286ecf07a837906797da7426e8416ac77fb6be
Adanna <aydeeleje@gmail.com>
2021-04-23 13:44:10 -0400

    first commit

--- car_inventory/authentication/routes.py
+++ car_inventory/authentication/routes.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from car_inventory.forms import UserLoginForm
+from car_inventory.models import User , db
+
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = UserLoginForm()
+    try:
+        if request.method == 'POST' and  form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+            user = User(email, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {email}', 'user-created')
+
+            return redirect(url_for('site.home'))
+    except:
+            raise Exception('Invalid Form of data: Please check your form input')
+
+    return render_template('signup.html', form = form)
 
 @auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
