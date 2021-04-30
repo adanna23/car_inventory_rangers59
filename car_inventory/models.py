84836cf60ae299c17de41d1795344661babbc3da
Adanna <aydeeleje@gmail.com>
2021-04-30 14:36:19 -0400

    first commit

--- car_inventory/models.py
+++ car_inventory/models.py
@@ -7,9 +7,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import secrets
 
-db = SQLAlchemy()
+from flask_login import LoginManager , UserMixin
 
-class User(db.Model):
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+ma = Marshmallow()
+
+@login_manager.user_loader
+def load_user(user_id): 
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True )
     first_name = db.Column(db.String(150), nullable = True, default ='')
     last_name = db.Column(db.String(150), nullable = True, default ='')
@@ -17,6 +27,7 @@ class User(db.Model):
     password = db.Column(db.String, nullable = False, default ='')
     token = db.Column(db.String, default ='' , unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default =datetime.utcnow)
+    car = db.relationship('Drone', backref = 'owner', lazy = True)
 
     def __init__(self,email,first_name = '' , last_name = '', id = '', password = '', token = ''):
         self.id = self.set_id()
@@ -38,3 +49,45 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been created and added to database'
+
+class Drone(db.Model):
+    id = db.Column(db.String, primary_key = True )
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(200), nullable = True)
+    price = db.Column(db.Numeric(precision=10, scale = 2))
+    miles = db.Column(db.Numeric(150), nullable = True)
+    fuel_capacity = db.Column(db.String(100), nullable = True)
+    max_speed = db.Column(db.String(100))
+    dimensions = db.Column(db.String(100))
+    weight = db.Column(db.String(50))
+    cost_of_prd = db.Column(db.Numeric(precision = 10, scale = 2))
+    series = db.Column(db.String(150))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+    def __init__( self, name, description, price, miles, fuel_capacity, max_speed,dimensions, weight, cost_of_prd, series, user_token, id = ''):
+        self.id = self.set_id()
+        self.name = name
+        self.description = description
+        self.price = price
+        self.miles = miles
+        self.fuel_capacity = fuel_capacity
+        self.max_speed = max_speed
+        self.dimensions = dimensions
+        self.weight = weight
+        self.cost_of_prd = cost_of_prd
+        self.series = series
+        self.user_token = user_token
+
+    def __repr__(self):
+        return f'The following Car has been added: {self.name}'
+
+    def set_id(self):
+        return secrets.token_urlsafe()
+
+class DroneSchema(ma.Schema):
+    class Meta:
+        fields = ['id','name','description', 'price', 'miles', 'fuel_capacity', 'max_speed', 'dimensions', 'weight', 'cost_of_prd', 'series', 'user_token']
+
+
+drone_schema = DroneSchema()
+drones_schema = DroneSchema(many = True)
